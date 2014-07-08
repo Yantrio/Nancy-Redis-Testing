72f3eddc705481075d78b93ab85229168d782f02
Yantrio <James@James-humphries.co.uk>
2014-07-08 10:28:46 +0100

    commit for richard

---
+++ Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Nancy.Bootstrapper;
+using Nancy.Session;
+using Nancy.TinyIoc;
+using Nancy;
+
+namespace Nancy_Redis_Testing
+{
+  public class Bootstrapper : DefaultNancyBootstrapper
+  {
+
+    protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+    {
+      base.ApplicationStartup(container, pipelines);
+
+      RedisBasedSessions.Enable(pipelines);
+    }
+  }
+}
---
+++ IndexModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Nancy_Redis_Testing
+{
+  using Nancy;
+
+  public class IndexModule : NancyModule
+  {
+    public IndexModule()
+    {
+      Get["/"] = parameters => View["index"];
+
+      Get["SessionTest/{key}"] = args =>
+      {
+        if (Request.Session[args.key] == null)
+        {
+          Request.Session[args.key] = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+        }
+        return Request.Session[args.key];
+      };
+    }
+  }
+}
